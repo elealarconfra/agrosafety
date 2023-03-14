95ee0283b193597ac6429f2730ae31c15503297e
elealarconfra <82480483+elealarconfra@users.noreply.github.com>
2023-03-13 20:35:12 -0500

    @input con onClick() organizar html  scss en pages

--- src/app/pages/pages/categories/categories.component.ts
+++ src/app/pages/pages/categories/categories.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Categories } from '../intefaces/categories.interface';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+  @Input() category!:Categories;
+
+  constructor(){}
+
+  ngOnInit():void{
+
+  }
+
+  onClick():void{
+    console.log('Click')
+  }
 
 }
